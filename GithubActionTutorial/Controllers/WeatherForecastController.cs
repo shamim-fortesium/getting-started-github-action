2a2c59ff51a536862afec185b66ea4e1a1635505
shamim-fortesium <94958058+shamim-fortesium@users.noreply.github.com>
2022-09-27 22:02:47 +0100

    Update WeatherForecastController.cs

--- GithubActionTutorial/Controllers/WeatherForecastController.cs
+++ GithubActionTutorial/Controllers/WeatherForecastController.cs
@@ -8,7 +8,8 @@ namespace GithubActionTutorial.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Humid"
+
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
