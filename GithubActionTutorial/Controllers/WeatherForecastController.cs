704361148da8bd7a60e39ab190df08373e194be9
shamim-fortesium <94958058+shamim-fortesium@users.noreply.github.com>
2022-09-28 16:08:55 +0100

    update list

--- GithubActionTutorial/Controllers/WeatherForecastController.cs
+++ GithubActionTutorial/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace GithubActionTutorial.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Humid"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Humid", "Very hot"
 
      };
 
